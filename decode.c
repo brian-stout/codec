861b954b787960e9d11b98b831cda4e7fad271c2
Brian Stout <brianjonstout@gmail.com>
2016-11-15 08:51:47 -0500

    Setting up structs for headers.

--- decode.c
+++ decode.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 #include <sysexits.h>
 
-int main(int argc, char *argv[])
+struct pcap_f_header
 {
-	if (argc != 2)
-	{
-		printf("ERROR, USAGE: [decode/encode] [~/<filename>.pcap]\n");
-		return EX_USAGE;		
-	}
 
-	FILE *fp = fopen(argv[1], "rb");
-	if (!fp)
-	{
-		printf("Unable to open file!\n");
-		return EX_USAGE;
-	}
 
-	fclose(fp);
+};
+
+struct pcap_p_header
+{
+
+};
+
+struct ethernet_frame
+{
+
+};
+
+struct ipv4_header
+{
+
+};
+
+struct UDP_header
+{
+
+};
+
+int
+main (int argc, char *argv[])
+{
+  if (argc != 2)
+    {
+      printf ("ERROR, USAGE: [decode/encode] [~/<filename>.pcap]\n");
+      return EX_USAGE;
+    }
+
+  FILE *fp = fopen (argv[1], "rb");
+  if (!fp)
+    {
+      printf ("Unable to open file!\n");
+      return EX_USAGE;
+    }
+
+  fclose (fp);
 }
--- encode.c
+++ encode.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <sysexits.h>
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("ERROR, USAGE: [decode/encode] [~/<filename>.pcap]\n");
-		return EX_USAGE;		
-	}
-	else
-	{
-		printf("%s\n", argv[1]);
-	}
+  if (argc != 2)
+    {
+      printf ("ERROR, USAGE: [decode/encode] [~/<filename>.pcap]\n");
+      return EX_USAGE;
+    }
+  else
+    {
+      printf ("%s\n", argv[1]);
+    }
 }
