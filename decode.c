b93e196ed12b79bd65304b305ff929da9e27f23b
Brian Stout <brianjonstout@gmail.com>
2016-11-15 09:57:48 -0500

    Declare the structs and fread them appropiately.

--- decode.c
+++ decode.c
@@ -77,8 +77,24 @@ main (int argc, char *argv[])
       printf ("Unable to open file!\n");
       return EX_USAGE;
     }
+	
+	struct pcap_global_header pcap_global_header;
+	fread(&pcap_global_header, sizeof(struct pcap_global_header), 1, fp);
 
-	printf("%zd \n", sizeof(pcap_global_header);
+	struct pcap_packet_header pcap_packet_header;
+	fread(&pcap_packet_header, sizeof(struct pcap_packet_header), 1, fp);
+
+	struct ethernet_frame ethernet_frame;
+	fread(&ethernet_frame, sizeof(struct ethernet_frame), 1, fp);
+
+	struct ipv4_header ipv4_header;
+	fread(&ipv4_header, sizeof(struct ipv4_header), 1, fp);
+
+	struct UDP_header UDP_header;
+	fread(&UDP_header, sizeof(struct UDP_header), 1, fp);
+
+	struct zerg_header zerg_header;
+	fread(&zerg_header, sizeof(struct zerg_header), 1, fp);
 
   fclose (fp);
 }
