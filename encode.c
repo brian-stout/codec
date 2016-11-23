0bfc2b57423a491dddac97e9bc8225db5932629c
Brian Stout <brianjonstout@gmail.com>
2016-11-23 09:28:19 -0500

    Add a  get_int_value function which grabs numbers from a line

--- encode.c
+++ encode.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <sysexits.h>
+#include <ctype.h>
+
+void get_int_value(FILE *fp)
 
 int
 main (int argc, char *argv[])
 {
   if (argc != 2)
     {
-      printf ("ERROR, USAGE: [decode/encode] [~/<filename>.pcap]\n");
+      printf ("ERROR, USAGE: [encode] [~/<filename>]\n");
       return EX_USAGE;
     }
   else
     {
-      printf ("%s\n", argv[1]);
+      FILE *fp = fopen(argv[1], "r");
     }
+
+    if (!fp)
+    {
+        printf("Unable to open file!\n");
+        return EX_USAGE;
+    }
+    
+    if (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+
+    }
+
+}
+
+void get_int_value(FILE *fp)
+{
+    char number_string[128];
+    char c;
+    size_t i = 0;
+    while(c = fgetc(fp) != EOF)
+    {
+        if(isdigit(c))
+        {
+            number_string[i] = c;
+            ++i;  
+        }
+    }
+    printf("%s\n", number_string);
 }
