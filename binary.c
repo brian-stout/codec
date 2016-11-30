0d4309c540b5c38e4980a04ee50b858c0ff052bf
Brian Stout <brianjonstout@gmail.com>
2016-11-30 09:32:51 -0500

    Remove most headers from binary.c because they're in binary.h already.

--- binary.c
+++ binary.c
@@ -1,14 +1,5 @@
 #include "binary.h"
 
-#include <ctype.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <sysexits.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
 uint64_t
 htonll(uint64_t i)
 {
