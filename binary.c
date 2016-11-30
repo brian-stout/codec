2ad50ff475c8ee22b32a50851ca1632715e6e0f7
Brian Stout <brianjonstout@gmail.com>
2016-11-30 09:31:02 -0500

    Ran indent on binary.c, also organized some of the code.

--- binary.c
+++ binary.c
@@ -9,34 +9,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-uint32_t
-float_to_bin(float a)
-{
-    union
-    {
-        float b;
-        uint32_t uint;
-    } u;
-    
-    u.b = a;
-    return u.uint;     
-}
-
-uint64_t doub_to_bin(double a)
-{
-    union
-    {
-        double b;
-        uint64_t uint;
-    } u;
-    u.b = a;
-    return u.uint;
-}
-
 uint64_t
 htonll(uint64_t i)
 {
-    //TODO: Save a few lines by removing an unnessecary variable
     uint32_t a;
     uint32_t b;
     uint64_t r = 0;
@@ -62,13 +37,9 @@ hton24(uint32_t i)
     return i;
 }
 
-
-
-//TODO: put in a header file for future use in other programs
 uint64_t
 ntohll(uint64_t i)
 {
-    //TODO: Save a few lines by removing an unnessecary variable
     uint32_t a;
     uint32_t b;
     uint64_t r = 0;
@@ -120,4 +91,30 @@ bin_to_doub(uint64_t a)
 
     u.b = a;
     return u.d;
+}
+
+uint32_t
+float_to_bin(float a)
+{
+    union
+    {
+        float b;
+        uint32_t uint;
+    } u;
+
+    u.b = a;
+    return u.uint;
+}
+
+uint64_t
+doub_to_bin(double a)
+{
+    union
+    {
+        double b;
+        uint64_t uint;
+    } u;
+
+    u.b = a;
+    return u.uint;
 }
